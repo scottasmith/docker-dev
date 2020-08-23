ecc8194119f655ffa8285d8084e8560b255cc996
Scott Smith <smitherz82@gmail.com>
2020-08-23 18:37:16 +0100

    Updated traefik and docker-compose

---
+++ setup-deps.sh
@@ -0,0 +1,7 @@
+docker network create --attachable web
+docker network create --attachable mail
+docker network create --attachable database
+docker volume create --name=mysql-data
+docker volume create --name=composer-cache
+docker volume create --name=yarn-cache
+docker volume create --name=npm-cache
