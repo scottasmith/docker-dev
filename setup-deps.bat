bbf2b597a0e857a8093abf6e5ad1fffcd4cd1539
Scott Smith <smitherz82@gmail.com>
2021-08-21 21:30:00 +0100

    Removed yarn volume in setup deps

--- setup-deps.bat
+++ setup-deps.bat
@@ -3,5 +3,4 @@ docker network create --attachable mail
 docker network create --attachable database
 docker volume create --name=mysql-data
 docker volume create --name=composer-cache
-docker volume create --name=yarn-cache
 docker volume create --name=npm-cache
