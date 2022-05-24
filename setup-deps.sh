2934da5942c77657f4266757bd58ef04227ecfdc
Scott Smith <smitherz82@gmail.com>
2022-05-24 20:32:19 +0100

    Added mongo and cleaned up

--- setup-deps.sh
+++ setup-deps.sh
@@ -1,6 +1,7 @@
 docker network create --attachable web
-docker network create --attachable mail
-docker network create --attachable database
 docker volume create --name=mysql-data
 docker volume create --name=composer-cache
 docker volume create --name=npm-cache
+
+docker run -v composer-cache:/cache/composer -v npm-cache:/cache/npm busybox chown -R 1000:1000 /cache
+
